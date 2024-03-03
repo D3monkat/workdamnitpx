2824d6030c1a2575dd0835e0177aee78a165f596
D3monkat <nsrp@newvisionrp.net>
2024-03-03 22:53:12 +0000

    add discord shit

--- [qb]/qb-smallresources/config.lua
+++ [qb]/qb-smallresources/config.lua
@@ -49,24 +49,24 @@ Config.AIResponse = {
 
 -- To Set This Up visit https://forum.cfx.re/t/how-to-updated-discord-rich-presence-custom-image/157686
 Config.Discord = {
-    isEnabled = false,                                     -- If set to true, then discord rich presence will be enabled
-    applicationId = '00000000000000000',                   -- The discord application id
+    isEnabled = true,                                     -- If set to true, then discord rich presence will be enabled
+    applicationId = '1166461881934946354',                   -- The discord application id
     iconLarge = 'logo_name',                               -- The name of the large icon
     iconLargeHoverText = 'This is a Large icon with text', -- The hover text of the large icon
     iconSmall = 'small_logo_name',                         -- The name of the small icon
     iconSmallHoverText = 'This is a Small icon with text', -- The hover text of the small icon
     updateRate = 60000,                                    -- How often the player count should be updated
     showPlayerCount = true,                                -- If set to true the player count will be displayed in the rich presence
-    maxPlayers = 48,                                       -- Maximum amount of players
+    maxPlayers = 64,                                       -- Maximum amount of players
     buttons = {
         {
-            text = 'First Button!',
-            url = 'fivem://connect/localhost:30120'
+            text = 'Join the Discord to play!',
+            url = 'https://discord.gg/Uukxh3TJMZ'
         },
-        {
-            text = 'Second Button!',
-            url = 'fivem://connect/localhost:30120'
-        }
+        -- {
+        --     text = 'Second Button!',
+        --     url = 'fivem://connect/localhost:30120'
+        -- }
     }
 }
 
