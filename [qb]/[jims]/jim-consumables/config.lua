18d0b32a7ed23e6b7a0da2eca90ea7587185fed2
D3monkat <nsrp@newvisionrp.net>
2024-03-03 22:40:21 +0000

    stuff for cat cafe

--- [qb]/[jims]/jim-consumables/config.lua
+++ [qb]/[jims]/jim-consumables/config.lua
@@ -8,8 +8,8 @@ Config = {
 	Debug = false,
 	Core = "qb-core",
 
-	Inv = "qb", -- set to "ox" if using ox_inventory
-	Notify = "qb",  -- set to "ox" if using ox_lib
+	Inv = "ox", -- set to "ox" if using ox_inventory
+	Notify = "ox",  -- set to "ox" if using ox_lib
 
 	UseProgbar = false,
 	ProgressBar = "qb", -- set to "ox" if using ox_lib
@@ -82,6 +82,60 @@ Config = {
 
 	},
 	Emotes = {
+		--Jim-CatCafe
+["uwu1"] = {
+	"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_sml_drink', PropBone = 28422, PropPlacement = {0.0, 0.0, 0.0, 0.0, 0.0, 130.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu2"] = {
+	"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_lrg_drink', PropBone = 28422, PropPlacement = {0.03, 0.0, -0.08, 0.0, 0.0, 130.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu3"] = {
+	"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_cup_straw', PropBone = 28422, PropPlacement = {0.0, 0.0, 0.0, 0.0, 0.0, 130.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu4"] = {
+	amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_mug', PropBone = 28422, PropPlacement = {0.0, 0.0, 0.0, 0.0, 0.0, 130.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu5"] = {
+	"mp_player_inteat@burger", "mp_player_int_eat_burger", "", AnimationOptions =
+	{ Prop = 'uwu_pastry', PropBone = 18905, PropPlacement = {0.16, 0.06, -0.03, -50.0, 16.0, 60.0},
+		EmoteMoving = true, }},
+["uwu6"] = {
+	"mp_player_inteat@burger", "mp_player_int_eat_burger", "", AnimationOptions =
+	{ Prop = 'uwu_cookie', PropBone = 18905, PropPlacement = {0.16, 0.08, -0.01, -225.0, 20.0, 60.0},
+		EmoteMoving = true, }},
+["uwu7"] = {
+	"mp_player_inteat@burger", "mp_player_int_eat_burger", "", AnimationOptions =
+	{ Prop = 'uwu_sushi', PropBone = 18905, PropPlacement = {0.18, 0.03, 0.02, -50.0, 16.0, 60.0},
+		EmoteMoving = true, }},
+["uwu8"] = {
+	"amb@world_human_seat_wall_eating@male@both_hands@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_eggroll', PropBone = 60309, PropPlacement = {0.10, 0.03, 0.08, -95.0, 60.0, 0.0},
+		EmoteMoving = true, }},
+["uwu9"] = {
+	"anim@scripted@island@special_peds@pavel@hs4_pavel_ig5_caviar_p1", "base_idle", "", AnimationOptions =
+	{ Prop = "uwu_salad_bowl", PropBone = 60309, PropPlacement = {0.0, 0.0300, 0.0100, 0.0, 0.0, 0.0},
+		SecondProp = 'uwu_salad_spoon', SecondPropBone = 28422, SecondPropPlacement = {0.0, 0.0 ,0.0, 0.0, 0.0, 0.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu10"] = {
+	"mp_player_inteat@burger", "mp_player_int_eat_burger", "", AnimationOptions =
+	{ Prop = 'uwu_sandy', PropBone = 18905, PropPlacement = {0.16, 0.08, 0.05, -225.0, 20.0, 60.0},
+		EmoteMoving = true, }},
+["uwu11"] = {
+	"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_cupcake', PropBone = 28422, PropPlacement = {0.0, 0.0, -0.03, 0.0, 0.0, 130.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu12"] = {
+	"amb@world_human_drinking@coffee@male@idle_a", "idle_c", "", AnimationOptions =
+	{ Prop = 'uwu_btea', PropBone = 28422, PropPlacement = {0.02, 0.0, -0.05, 0.0, 0.0, 130.0},
+		EmoteLoop = true, EmoteMoving = true, }},
+["uwu13"] = {
+	"mp_player_inteat@burger", "mp_player_int_eat_burger", "", AnimationOptions =
+	{ Prop = 'uwu_gdasik', PropBone = 18905, PropPlacement = {0.16, 0.08, 0.02, -225.0, 20.0, 60.0},
+		EmoteMoving = true, }},
 		["drink"] = {"mp_player_intdrink", "loop_bottle", "Drink", AnimationOptions =
 			{ Prop = "prop_ld_flow_bottle", PropBone = 18905, PropPlacement = {0.12, 0.008, 0.03, 240.0, -60.0},
 				EmoteMoving = true, EmoteLoop = true, }},
--- [qb]/[jims]/jim-consumables/fxmanifest.lua
+++ [qb]/[jims]/jim-consumables/fxmanifest.lua
@@ -6,6 +6,7 @@ fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
-shared_scripts { 'config.lua', 'shared/*.lua' }
+shared_scripts { 'config.lua', 'shared/*.lua', 
+'@ox_lib/init.lua', }
 client_scripts { 'client/*.lua', }
 server_scripts { 'server/*.lua' }
