cc4e1fb851b855e91a31b08ee310aba1187f19a2
D3monkat <nsrp@newvisionrp.net>
2024-03-04 01:55:50 +0000

    f

--- [ox]/ox_inventory/init.lua
+++ [ox]/ox_inventory/init.lua
@@ -18,7 +18,7 @@ shared = {
     playerslots = GetConvarInt('inventory:slots', 50),
     playerweight = GetConvarInt('inventory:weight', 30000),
     target = GetConvarInt('inventory:target', 0) == 1,
-    police = json.decode(GetConvar('inventory:police', '["police", "sheriff"]')),
+    police = json.decode(GetConvar('inventory:police', '["police", "bcso", "sasp"]')),
 }
 
 do
