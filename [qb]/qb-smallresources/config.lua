830ffffc85b4217ffc0558b474239f572ef6a2b2
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-03-03 18:42:06 -0800

    Update config.lua

--- [qb]/qb-smallresources/config.lua
+++ [qb]/qb-smallresources/config.lua
@@ -1,6 +1,7 @@
 Config = {}
+Meow = {}
 -- nearestpostal
-config = {
+Meow = {
     -- enables version checking (if this is enabled and there is no new version it won't display a message anyways)
     versionCheck = true,
 
