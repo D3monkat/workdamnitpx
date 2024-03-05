cf0e982a67fa4a079e9349ab14a3bb4be09ef318
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-03-04 23:26:03 -0600

    add shells

---
+++ [defaultmaps]/K4MB1-AllShellsSubPack/fxmanifest.lua
@@ -0,0 +1,13 @@
+lua54 'yes'
+
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+      'stream/allshellsbundle_k4mb1.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/allshellsbundle_k4mb1.ytyp'
+dependency '/assetpacks'
