c4232989503c5b33f031b09fb8fe540d3a60ea9f
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-03-08 20:40:05 -0600

    f

---
+++ [vehicles]/SAST/data/seadoohauler/fxmanifest.lua
@@ -0,0 +1,20 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Code 3 Development'
+version '1.0.0'
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta'
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
