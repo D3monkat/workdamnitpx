3a047e6dbda1fcf18fd0629f214d695ac958cc7c
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-03-09 23:42:35 -0600

    Update server.lua

--- [qb]/[ps]/ps-hud/server.lua
+++ [qb]/[ps]/ps-hud/server.lua
@@ -5,7 +5,7 @@ RegisterNetEvent('hud:server:GainStress', function(amount)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local newStress
-    if not Player or (Config.DisablePoliceStress and Player.PlayerData.job.type == 'leo' or Player.PlayerData.job.type = 'ems') then return end
+    if not Player or (Config.DisablePoliceStress and Player.PlayerData.job.type == 'leo' or Player.PlayerData.job.type == 'ems') then return end
     if not ResetStress then
         if not Player.PlayerData.metadata['stress'] then
             Player.PlayerData.metadata['stress'] = 0
