c19bcedbbc908969f407c27e97de861a123502c6
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-03-10 04:12:53 -0500

    f

--- [qb]/qb-spawn/fxmanifest.lua
+++ [qb]/qb-spawn/fxmanifest.lua
@@ -8,7 +8,7 @@ version '1.0.9'
 shared_scripts {
 	'config.lua',
 	-- '@qb-apartments/config.lua' -- If you use ps-housing, comment this line
-    '@ps-housing/shared/config.lua' -- If you use qb-houses, comment this line
+    -- '@ps-housing/shared/config.lua' -- If you use qb-houses, comment this line
 }
 
 client_script 'client.lua'
