c0dd675add239c79eb72135814130dec5f5bcc14
D3monkat <122725955+D3monkat@users.noreply.github.com>
2024-03-11 02:16:39 -0500

    gf

--- [defaultmaps]/ManWithThePan/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'adamant'
-game 'gta5'
-
-this_is_a_map 'yes'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/tmwapsign.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/hane_subway.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/hane_subway_logo.ytyp'
